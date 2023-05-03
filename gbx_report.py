871b212b1c05660f5ccec7153ad33843396abc38
Sergio Hernandez-Acosta <79073281+sergiojhernandezacosta@users.noreply.github.com>
2023-05-03 13:34:09 -0500

    Create gbx_report.py

---
+++ gbx_report.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+location = r"C:\Users\shacosta\Desktop\kNeighborGBX.xlsx"
+excel = pd.read_excel(location)
+
+df = pd.DataFrame(excel)
+
+mod_col = df.iloc[:,0].dropna()
+eff_col = df.iloc[:,2].dropna()
+premium_col = df.iloc[:,4].dropna()
+
+# Calculate mode and occurrence count for each column
+columns = ['GBX for Price-Moderate:','GBX for Price-Efficient:','GBX for Price-Premium:']
+for column in columns:
+    col = df[column].dropna()
+    first_three_letters_mode = col.astype(str).str[:3].mode()
+    mode_count = col.astype(str).str[:3].value_counts().max()
+
+    print("Most common gearbox brand in",column, first_three_letters_mode[0])
+    print("Occurrence count:", mode_count)
+    print()
